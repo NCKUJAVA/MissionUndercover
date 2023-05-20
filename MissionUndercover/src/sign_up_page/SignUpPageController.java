424712929706b9790dfa17a685afa57f6fa063e7
aaronXctl <aaron.start.up.99@gmail.com>
2023-05-21 00:07:35 +0800

    aaron mergesolved shangyuan

--- MissionUndercover/src/sign_up_page/SignUpPageController.java
+++ MissionUndercover/src/sign_up_page/SignUpPageController.java
@@ -186,7 +186,7 @@ public class SignUpPageController
 			String driver = "com.mysql.cj.jdbc.Driver";
 			String url = "jdbc:mysql://localhost:3306/MissionUndercover_DB";
 			String username = "root";
-			String password = "F74086250";
+			String password = "240813333";
 			Class.forName(driver);
 			Connection conn = DriverManager.getConnection(url, username, password);
 			System.out.println("Connecting successfully!");
