4fd6554c174e115dd7faf17e91983084d0f3b0a5
aaronXctl <aaron.start.up.99@gmail.com>
2023-05-20 23:40:22 +0800

    aaron 0520 mainwindowconnect

--- MissionUndercover/src/mainwindow_page/Main.java
+++ MissionUndercover/src/mainwindow_page/Main.java
@@ -1,18 +1,20 @@
-package application;
+package mainwindow_page;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	
+	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			Parent root = (Parent) FXMLLoader.load(getClass().getResource("/mainwindow_page/mainwindow.fxml"));			
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("mainwindow.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -20,7 +22,7 @@ public class Main extends Application {
 		}
 	}
 	
-//	public static void main(String[] args) {
-//		launch(args);
-//	}
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
--- MissionUndercover/src/module-info.java
+++ MissionUndercover/src/module-info.java
@@ -1,5 +1,10 @@
 module MissionUndercover {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    
+    opens application to javafx.graphics, javafx.fxml;
+    opens mainwindow_page to javafx.graphics, javafx.fxml;
+    opens maininfo_page to javafx.graphics, javafx.fxml;
+    opens mainshop_page to javafx.graphics, javafx.fxml;
 }
