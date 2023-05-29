3db1bd354e87018ac2ed5ef0b324d9e5e26cb2e3
F74086250 <aeiou95048@gmail.com>
2023-05-29 23:46:51 +0800

    shang push  merge crazy settlement_page 20230529

--- MissionUndercover/src/start_page/StartPageController.java
+++ MissionUndercover/src/start_page/StartPageController.java
@@ -112,7 +112,7 @@ public class StartPageController
 				}
 				StartPage.player.resetNowString();
 				//Parent root = FXMLLoader.load(getClass().getResource("/mainwindow_page/mainwindow.fxml"));
-				Parent root = FXMLLoader.load(getClass().getResource("/settlement_page/SettlementPageFXML.fxml"));
+				Parent root = FXMLLoader.load(getClass().getResource("/mainwindow_page/mainwindow.fxml"));
 		        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
 		        scene = new Scene(root);
 		        stage.setScene(scene);
